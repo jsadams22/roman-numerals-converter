da01844422b573069879ef161fd1f1e7b28c9f54
Josh Adams <adams.joshua.s@gmail.com>
2025-07-26 16:21:26 -0600

    Hook the frontend up to the backend
    Adds a submit handler and some state variables to hold our values, as
    well as a display for the value we got back.
    
    The state variables hold the user's input (only lightly validated so
    far with a NumberField) and the result we got back from the backend.
    
    Our submit handler calls our conversion service and passes the user's
    input, handling errors with a console log for now.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,35 @@
 import './App.css'
-import {Button, defaultTheme, Form, Provider, TextField, View} from "@adobe/react-spectrum";
+import {Button, defaultTheme, Form, NumberField, Provider, View} from "@adobe/react-spectrum";
+import {useState} from "react";
+import {convertToRomanNumerals} from "./services/roman-numerals.ts";
 
 const App = () => {
+    const [romanNumeral, setRomanNumeral] = useState<string>('');
+    const [integerValue, setIntegerValue] = useState<number>(0);
+
+    const onSubmit = async () => {
+        try {
+            const romanNumeral = await convertToRomanNumerals(integerValue);
+            setRomanNumeral(romanNumeral);
+        } catch (error) {
+            // TODO: Add better error handling
+            console.error(error);
+        }
+    };
+
     return (
         <Provider theme={defaultTheme}>
             <View>
                 Roman numeral converter
                 <Form maxWidth="size-4600">
-                    <TextField label="Enter a number"/>
-                    <Button type="submit" variant="primary">Convert to roman numeral</Button>
+                    <NumberField onChange={setIntegerValue} label="Enter a number"/>
+                    <Button type="button" variant="primary" onPress={onSubmit}>Convert to roman numeral</Button>
                 </Form>
+                { romanNumeral !== '' &&
+                    <View>
+                        Roman Numeral: {romanNumeral}
+                    </View>
+                }
             </View>
         </Provider>
     );
