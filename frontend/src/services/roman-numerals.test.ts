2aaaf6e47af33222d96e3a715fe2371211e53914
Josh Adams <adams.joshua.s@gmail.com>
2025-07-26 17:14:42 -0600

    Make environment configurable
    Added a production, development, and test environment file. Made the
    convertRomanNumerals service use env vars from the new environment
    files. Added a test that checks that the values from the environments
    are used appropriately.
    
    Added a test to make sure that we're sending the query parameter to the
    backend.
    
    Tweaked the vite config to look for the new env files.

--- frontend/src/services/roman-numerals.test.ts
+++ frontend/src/services/roman-numerals.test.ts
@@ -1,16 +1,15 @@
-import {vi, test, describe, afterEach, expect} from 'vitest';
+import {vi, test, describe, expect, beforeEach} from 'vitest';
 import axios from 'axios';
 import {convertToRomanNumerals} from "./roman-numerals.ts";
 
 describe('Convert to roman numerals', () => {
     const getSpy = vi.spyOn(axios, 'get');
-    afterEach(() => {
-        vi.restoreAllMocks();
+
+    beforeEach(async () => {
+        getSpy.mockImplementation(() => Promise.resolve({data: 'XVI'}));
     });
 
     test('calls backend for roman numerals', async () => {
-        getSpy.mockImplementation(() => Promise.resolve({data: 'XVI'}));
-
         const response = await convertToRomanNumerals(123);
         expect(response).toEqual('XVI');
         expect(getSpy).toHaveBeenCalled();
@@ -20,5 +19,29 @@ describe('Convert to roman numerals', () => {
         getSpy.mockImplementation(() => Promise.reject(new Error('error')));
 
         await expect(convertToRomanNumerals(123)).rejects.toThrow('error');
+    });
+
+    test('passes query to backend', async () => {
+        const fakeQuery = 123456;
+        const response = await convertToRomanNumerals(fakeQuery);
+
+        expect(response).toEqual('XVI');
+        expect(getSpy).toHaveBeenCalledWith(expect.stringContaining(`query=${fakeQuery}`));
+    });
+
+    test('uses configuration for backend address/port', async () => {
+        const fakeAddress = 'http://fake_location.com';
+        const fakePort = 1234;
+        vi.stubEnv('VITE_SERVER_ADDRESS', fakeAddress)
+        vi.stubEnv('VITE_SERVER_PORT', fakePort.toString());
+
+        const expectedFakeUrl = `${fakeAddress}:${fakePort}/romannumeral`;
+
+        const response = await convertToRomanNumerals(123);
+
+        expect(response).toEqual('XVI');
+        expect(getSpy).toHaveBeenCalledWith(expect.stringMatching(new RegExp(`^${expectedFakeUrl}`)));
+
+        vi.unstubAllEnvs();
     });
 });
--- frontend/src/services/roman-numerals.ts
+++ frontend/src/services/roman-numerals.ts
@@ -1,16 +1,12 @@
 import axios from 'axios';
 
-// TODO: Make these configurable
-const SERVER_ADDRESS = 'http://localhost';
-const SERVER_PORT = 3000;
-
 /**
  * Converts a number to roman numerals by calling the backend. Intentionally does not do much input validation here;
  * that is left up to the caller.
  * @param toConvert number that should be converted to Roman numerals
  */
 const convertToRomanNumerals = async (toConvert: number): Promise<string> => {
-    const response = await axios.get<string>(`${SERVER_ADDRESS}:${SERVER_PORT}/romannumeral?query=${toConvert}`);
+    const response = await axios.get<string>(`${import.meta.env.VITE_SERVER_ADDRESS}:${import.meta.env.VITE_SERVER_PORT}/romannumeral?query=${toConvert}`);
 
     return response.data
 };
