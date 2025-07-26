149c7b2094712e315f7687c3dccb592d784c0578
Josh Adams <adams.joshua.s@gmail.com>
2025-07-26 16:20:07 -0600

    Add basic CORS handling
    Since this is a local project, I'm not going to do anything fancy here,
    just accept everything for CORs.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+        allowedHeaders: '*',
+        origin: '*',
+        exposedHeaders: '*',
+        credentials: true,
+    });
+
     await app.listen(process.env.PORT ?? 3000);
 }
 
