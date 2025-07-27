3c7a906a4390d1f3064b4bfa3829645980cdcc8a
Josh Adams <adams.joshua.s@gmail.com>
2025-07-26 23:50:35 -0600

    Incorporate input validation pipes, flesh out E2E tests
    Add some validation pipes to our romannumeral endpoint, along with a
    descriptive comment explaining how it works.
    
    The pipes will validate that the query parameter is set, is an integer,
    and that it is within 1-3999, using our new IntRagnePipe.
    
    Reformats the E2Es to match our code style, and adds a bunch of new
    tests that validate that we get back either a 400 or 200 status back
    depending on if the input is invalid or valid, respectively.
    
    The tests are not yet validating for correct roman numeral conversion,
    since we haven't implemented that functionality.

--- backend/src/roman-numerals/roman-numerals.controller.ts
+++ backend/src/roman-numerals/roman-numerals.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
+import { IntRangePipe } from './pipes/int-range.pipe';
 
 @Controller('romannumeral')
 export class RomanNumeralsController {
+    /**
+     * The `romannumeral` endpoint expects to receive a single integer query parameter named `query` that will be
+     * converted to a roman numeral and returned, along with the input.
+     * @param query Must be an integer between 1 and 3999. If not, a 400 Bad Request will be returned
+     */
     @Get()
-    convertToRoman(@Query('query') query: string) {
+    convertToRoman(@Query('query', ParseIntPipe, new IntRangePipe(1, 3999)) query: string) {
         console.log(`Got query for ${query}`);
         return query;
     }
--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -2,24 +2,63 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+    let app: INestApplication<App>;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+    it('/ (GET)', () => {
+        return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+    });
+
+    describe('/romannumeral (GET)', () => {
+        describe('when the query parameter is not valid', () => {
+            it('returns a bad request when the query parameter is missing', () => {
+                return request(app.getHttpServer()).get('/romannumeral').expect(400);
+            });
+
+            it('returns a bad request when the query parameter is empty', () => {
+                return request(app.getHttpServer()).get('/romannumeral?query=').expect(400);
+            });
+
+            it('returns a bad request when the query parameter is a float', () => {
+                return request(app.getHttpServer()).get('/romannumeral?query=1.1').expect(400);
+            });
+
+            it('returns a bad request when the query parameter is a negative number', () => {
+                return request(app.getHttpServer()).get('/romannumeral?query=-1').expect(400);
+            });
+
+            it('returns a bad request when the query parameter is a number greater than 3999', () => {
+                return request(app.getHttpServer()).get('/romannumeral?query=4000').expect(400);
+            });
+
+            it('returns a bad request when the query parameter is a string', () => {
+                return request(app.getHttpServer()).get('/romannumeral?query=abc').expect(400);
+            });
+        });
+
+        describe('when the query parameter is valid', () => {
+            it('returns OK when the query parameter is at the lower limit', () => {
+                return request(app.getHttpServer()).get('/romannumeral?query=1').expect(200);
+            });
+
+            it('returns OK when the query parameter is at the upper limit', () => {
+                return request(app.getHttpServer()).get('/romannumeral?query=3999').expect(200);
+            });
+
+            it('returns OK when the query parameter is in the middle', () => {
+                return request(app.getHttpServer()).get('/romannumeral?query=1500').expect(200);
+            });
+        });
+    });
 });
