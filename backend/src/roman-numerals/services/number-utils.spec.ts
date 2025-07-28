9e0d8f1df1f698b785b42c1883011396b20761dd
Josh Adams <adams.joshua.s@gmail.com>
2025-07-28 17:29:44 -0600

    Flesh out testing and support for getDigits utility
    Thought of a few cases that could end up getting passed to a utility
    function like this in a hypothetical future and thought I should handle
    them, since it was fairly easy.
    
    Note that this isn't a perfect solution; other possibilities I
    considered were to respond with an error, or make the representation of
    the returned digits handle more cases. I didn't feel that either of
    those were particularly warranted in this case, since, as currently
    written, neither of these should end up getting passed here anyway, and
    if in the future they are, a doc comment should be enough to warn a
    user, and other, larger chunks of this code would need be modified to
    handle them.

--- backend/src/roman-numerals/services/number-utils.spec.ts
+++ backend/src/roman-numerals/services/number-utils.spec.ts
@@ -8,4 +8,13 @@ describe('NumberUtils', () => {
         expect(getDigits(123)).toEqual([3, 2, 1]);
         expect(getDigits(1234)).toEqual([4, 3, 2, 1]);
     });
+
+    it('converts negative numbers to positive', () => {
+        expect(getDigits(-10)).toEqual([0, 1]);
+    });
+
+    it('handles floats by rounding down', () => {
+        expect(getDigits(5.5)).toEqual([5]);
+        expect(getDigits(52.0)).toEqual([2, 5]);
+    });
 });
--- backend/src/roman-numerals/services/number-utils.ts
+++ backend/src/roman-numerals/services/number-utils.ts
@@ -1,11 +1,21 @@
 /**
  * Separates the digits in `input` into an array of the form [ones, tens, hundreds, thousands, ...]
+ * Negative numbers are not explicitly represented, and instead will be converted to positive numbers.
+ * Floats are likewise not represented and will be floored first, then have their digits processed.
  * @param input Number to separate into component digits
  */
 export const getDigits = (input: number): number[] => {
     const digits: number[] = [];
 
     let choppedInput = input;
+
+    if (choppedInput < 0) {
+        choppedInput *= -1;
+    }
+
+    if (!Number.isInteger(choppedInput)) {
+        choppedInput = Math.floor(choppedInput);
+    }
 
     // Run through our input, chopping it down, until we're out of digits
     while (choppedInput > 0) {
