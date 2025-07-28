5adb3180ed0ada66fc504943e5de25f66cc123ec
Josh Adams <adams.joshua.s@gmail.com>
2025-07-27 18:01:21 -0600

    Fix unit tests after adding Winston
    Forgot to come back and mock the Winston module for the tests.
    
    Included the @golevelup/ts-jest package to make mocking it a little
    easier. I've used this package quite a bit in the past and like the
    utilities it adds. This tweak will make it so we can just auto-mock the
    whole logging service without having to do something like add a
    `jest.fn()` for each method on the logger if we accidentally add a call
    to one later.

--- backend/src/roman-numerals/roman-numerals.controller.spec.ts
+++ backend/src/roman-numerals/roman-numerals.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RomanNumeralsController } from './roman-numerals.controller';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { LoggerService } from '@nestjs/common';
+import { createMock } from '@golevelup/ts-jest';
 
 describe('RomanNumeralsController', () => {
     let controller: RomanNumeralsController;
@@ -7,6 +10,12 @@ describe('RomanNumeralsController', () => {
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [RomanNumeralsController],
+            providers: [
+                {
+                    provide: WINSTON_MODULE_PROVIDER,
+                    useValue: createMock<LoggerService>(),
+                },
+            ],
         }).compile();
 
         controller = module.get<RomanNumeralsController>(RomanNumeralsController);
