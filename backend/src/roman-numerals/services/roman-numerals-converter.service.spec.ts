7b9a1d0ffb6de360dd20c4346d8e91308dfca97f
Josh Adams <adams.joshua.s@gmail.com>
2025-07-28 17:20:09 -0600

    Add some more unit tests for numeral converter
    Thought I should at least test all of the hardcoded examples from our
    lookup table, and decided to throw in a few more.
    
    This is not comprehensive, but represents a good spread.

--- backend/src/roman-numerals/services/roman-numerals-converter.service.spec.ts
+++ backend/src/roman-numerals/services/roman-numerals-converter.service.spec.ts
@@ -28,34 +28,59 @@ describe('RomanNumeralsConverterService', () => {
     describe('convertToRoman', () => {
         it('should convert single digit numbers correctly', () => {
             expect(service.convertToRoman(1)).toBe('I');
+            expect(service.convertToRoman(2)).toBe('II');
+            expect(service.convertToRoman(3)).toBe('III');
+            expect(service.convertToRoman(4)).toBe('IV');
             expect(service.convertToRoman(5)).toBe('V');
+            expect(service.convertToRoman(6)).toBe('VI');
+            expect(service.convertToRoman(7)).toBe('VII');
+            expect(service.convertToRoman(8)).toBe('VIII');
             expect(service.convertToRoman(9)).toBe('IX');
         });
 
         it('should convert double digit numbers correctly', () => {
             expect(service.convertToRoman(10)).toBe('X');
-            expect(service.convertToRoman(12)).toBe('XII');
-            expect(service.convertToRoman(11)).toBe('XI');
-            expect(service.convertToRoman(14)).toBe('XIV');
-            expect(service.convertToRoman(15)).toBe('XV');
             expect(service.convertToRoman(20)).toBe('XX');
+            expect(service.convertToRoman(30)).toBe('XXX');
             expect(service.convertToRoman(40)).toBe('XL');
             expect(service.convertToRoman(50)).toBe('L');
+            expect(service.convertToRoman(60)).toBe('LX');
+            expect(service.convertToRoman(70)).toBe('LXX');
+            expect(service.convertToRoman(80)).toBe('LXXX');
             expect(service.convertToRoman(90)).toBe('XC');
+
+            // Spot check a few other double-digit numbers
+            expect(service.convertToRoman(11)).toBe('XI');
+            expect(service.convertToRoman(12)).toBe('XII');
+            expect(service.convertToRoman(13)).toBe('XIII');
+            expect(service.convertToRoman(14)).toBe('XIV');
+            expect(service.convertToRoman(15)).toBe('XV');
+            expect(service.convertToRoman(33)).toBe('XXXIII');
+            expect(service.convertToRoman(42)).toBe('XLII');
+            expect(service.convertToRoman(56)).toBe('LVI');
         });
 
         it('should convert triple digit numbers correctly', () => {
             expect(service.convertToRoman(100)).toBe('C');
-            expect(service.convertToRoman(101)).toBe('CI');
-            expect(service.convertToRoman(102)).toBe('CII');
+            expect(service.convertToRoman(200)).toBe('CC');
+            expect(service.convertToRoman(300)).toBe('CCC');
             expect(service.convertToRoman(400)).toBe('CD');
             expect(service.convertToRoman(500)).toBe('D');
+            expect(service.convertToRoman(600)).toBe('DC');
+            expect(service.convertToRoman(700)).toBe('DCC');
+            expect(service.convertToRoman(800)).toBe('DCCC');
             expect(service.convertToRoman(900)).toBe('CM');
+
+            // Spot check a few other 3 digit numbers
+            expect(service.convertToRoman(101)).toBe('CI');
+            expect(service.convertToRoman(102)).toBe('CII');
+            expect(service.convertToRoman(444)).toBe('CDXLIV');
         });
 
         it('should convert four digit numbers correctly', () => {
             expect(service.convertToRoman(1000)).toBe('M');
             expect(service.convertToRoman(2000)).toBe('MM');
+            expect(service.convertToRoman(3000)).toBe('MMM');
         });
 
         it('should handle complex numbers correctly', () => {
