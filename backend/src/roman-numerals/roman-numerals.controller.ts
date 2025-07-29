2240725a9d93bc36dabbbcd9acdf4a70484e8793
Josh Adams <adams.joshua.s@gmail.com>
2025-07-29 16:45:28 -0600

    Add a custom metric
    This is mostly just to demonstrate how it could be done. The traces
    functionality is already gathering metrics about how long our requests
    are taking and what our responses are (i.e. error vs OK). This shows how
    we could add another metric for custom things we wanted to measure.

--- backend/src/roman-numerals/roman-numerals.controller.ts
+++ backend/src/roman-numerals/roman-numerals.controller.ts
@@ -12,13 +12,36 @@ import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
 import { ConversionResult } from './model/conversion-result';
 import { RomanNumeralsConverterService } from './services/roman-numerals-converter.service';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
+import { MeterProvider, PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { Attributes, Histogram } from '@opentelemetry/api';
 
 @Controller('romannumeral')
 export class RomanNumeralsController {
+    private numeralHistogram: Histogram<Attributes>;
+
     constructor(
         private readonly converterService: RomanNumeralsConverterService,
         @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
-    ) {}
+    ) {
+        // If we want more custom metrics than this, break it out into a separate utility or possibly a module so we can
+        // dependency inject it
+        const metricExporter = new OTLPMetricExporter({
+            timeoutMillis: 5000,
+        });
+
+        const metricReader = new PeriodicExportingMetricReader({
+            exporter: metricExporter,
+            exportIntervalMillis: 10000,
+        });
+
+        const meterProvider = new MeterProvider({ readers: [metricReader] });
+
+        const meter = meterProvider.getMeter('roman-numerals-service');
+        this.numeralHistogram = meter.createHistogram('roman_numeral_length', {
+            description: 'Number of digits in Roman numerals converted',
+        });
+    }
 
     /**
      * The `romannumeral` endpoint expects to receive a single integer query parameter named `query` that will be
@@ -39,11 +62,13 @@ export class RomanNumeralsController {
                 this.logger.error(`Input ${query} is out of range and was not caught by input validators!`);
                 throw new BadRequestException(error.message);
             } else {
+                // Anything we don't recognize rethrow as an internal error (500)
                 throw new InternalServerErrorException(error);
             }
         }
 
-        // TODO: Handle any errors from the conversion
+        // Custom metric to record something not directly related to handling requests/timings
+        this.numeralHistogram.record(romanNumeral.length);
 
         // Fill out the response object for the API
         this.logger.info(`Converted ${query} to ${romanNumeral}`);
