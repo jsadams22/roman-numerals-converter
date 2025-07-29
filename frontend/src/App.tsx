de067706b485ff5fda5952a46336c22a5c005947
Josh Adams <adams.joshua.s@gmail.com>
2025-07-28 21:22:02 -0600

    Handle errors on frontend, improve UI appearance
    Implements some proper error handling in our API requests and tweaks the
    UI to look cleaner, less cramped.
    
    Added the styled-components package to the frontend, which is used to
    provide a nice, structured way to apply some styles to things like our
    error message and our roman numeral display.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,80 @@
 import './App.css'
-import {Button, defaultTheme, Form, NumberField, Provider, View} from "@adobe/react-spectrum";
+import {
+    Button,
+    defaultTheme,
+    Form,
+    NumberField,
+    Provider,
+    View,
+    Text,
+    Flex,
+    Heading
+} from "@adobe/react-spectrum";
 import {useState} from "react";
 import {convertToRomanNumerals} from "./services/roman-numerals.ts";
+import styled from "styled-components";
 
 const App = () => {
     const [romanNumeral, setRomanNumeral] = useState<string>('');
     const [integerValue, setIntegerValue] = useState<number>(0);
+    const [error, setError] = useState<string>('');
 
     const onSubmit = async () => {
         try {
             const romanNumeral = await convertToRomanNumerals(integerValue);
+            setError('');
             setRomanNumeral(romanNumeral);
         } catch (error) {
-            // TODO: Add better error handling
-            console.error(error);
+            setRomanNumeral('');
+            if (error instanceof Error) {
+                setError(error.message);
+            } else {
+                setError('Unknown error');
+            }
         }
     };
 
     return (
         <Provider theme={defaultTheme}>
-            <View>
-                Roman numeral converter
-                <Form maxWidth="size-4600">
-                    <NumberField onChange={setIntegerValue} label="Enter a number"/>
-                    <Button type="button" variant="primary" onPress={onSubmit}>Convert to roman numeral</Button>
-                </Form>
-                { romanNumeral !== '' &&
-                    <View>
-                        Roman Numeral: {romanNumeral}
-                    </View>
-                }
+            <View padding="size-700" width="size-4600" height="size-3400">
+                <Flex direction="column" gap="size-300" alignItems="start">
+                    <Heading level={2}>Roman numeral converter</Heading>
+                    <Form width="100%">
+                        <NumberField onChange={setIntegerValue} label="Enter a number" hideStepper />
+                        <Button type="button" maxWidth="size-3000" variant="primary" marginTop="size-200" onPress={onSubmit}>Convert to roman numeral</Button>
+                    </Form>
+                    { romanNumeral !== '' &&
+                        <OutputWrapper>
+                            <Text>
+                                Roman numeral: {romanNumeral}
+                            </Text>
+                        </OutputWrapper>
+                    }
+                    { error !== '' &&
+                        <ErrorWrapper>
+                            <Text>
+                                Something went wrong:<br />
+                                {error}
+                            </Text>
+                        </ErrorWrapper>
+                    }
+                </Flex>
             </View>
         </Provider>
     );
 };
 
-export default App
+const OutputWrapper = styled.div`
+    font-weight: bold;
+    font-size: 110%;
+`;
+
+const ErrorWrapper = styled.div`
+    color: red;
+    padding: 10px;
+    text-align: left;
+    font-size: 110%;
+    font-weight: bold;
+`;
+
+export default App;
--- frontend/src/services/roman-numerals.ts
+++ frontend/src/services/roman-numerals.ts
@@ -1,15 +1,31 @@
-import axios from 'axios';
+import axios, {AxiosError} from 'axios';
 import type {ConversionResult} from "../model/conversion-result.ts";
 
+interface RomanNumeralError {
+    error: string;
+    message: string;
+    statusCode: number;
+}
+
 /**
- * Converts a number to roman numerals by calling the backend. Intentionally does not do much input validation here;
- * that is left up to the caller.
+ * Converts a number to roman numerals by calling the backend. Handles validating the response and translating it into
+ * either the roman numeral output or a nice error message if an error is received from the API.
  * @param toConvert number that should be converted to Roman numerals
  */
 const convertToRomanNumerals = async (toConvert: number): Promise<string> => {
-    const response = await axios.get<ConversionResult>(`${import.meta.env.VITE_SERVER_ADDRESS}:${import.meta.env.VITE_SERVER_PORT}/romannumeral?query=${toConvert}`);
+    try {
+        const response = await axios.get<ConversionResult>(`${import.meta.env.VITE_SERVER_ADDRESS}:${import.meta.env.VITE_SERVER_PORT}/romannumeral?query=${toConvert}`);
 
-    return response.data.output;
+        return response.data.output;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            // Check if this is an axios error, and if so, use the nice type that our backend returns
+            const romanNumeralsError = error as AxiosError<RomanNumeralError>;
+            throw new Error(romanNumeralsError.response?.data.message);
+        } else {
+            throw error;
+        }
+    }
 };
 
 export { convertToRomanNumerals };
