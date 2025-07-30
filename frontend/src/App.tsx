9869eecb290447d2fc16ca77457002143047398a
Josh Adams <adams.joshua.s@gmail.com>
2025-07-29 19:24:25 -0600

    Set max and min input for number field
    Since we're using a number field here that provides us a minimum and
    maximum value, let's use those. The backend will do error checking as
    well, but this helps the user not enter the wrong thing.
    
    Also add a hint to the user on the label what the acceptable values are,
    since the number field just clamps the input to the acceptable range
    automatically after the field loses focus.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -40,7 +40,7 @@ const App = () => {
                 <Flex direction="column" gap="size-300" alignItems="start">
                     <Heading level={2}>Roman numeral converter</Heading>
                     <Form width="100%">
-                        <NumberField onChange={setIntegerValue} label="Enter a number" hideStepper />
+                        <NumberField onChange={setIntegerValue} minValue={1} maxValue={3999} label="Enter a number (1-3,999)" hideStepper />
                         <Button type="button" maxWidth="size-3000" variant="primary" marginTop="size-200" onPress={onSubmit}>Convert to roman numeral</Button>
                     </Form>
                     { romanNumeral !== '' &&
